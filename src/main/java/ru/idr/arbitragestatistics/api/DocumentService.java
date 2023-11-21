9faa8af791fa1a111bd194ebd7c0f59b06d8aa07
Maxim Rusanov <rusanoph@id-r.ru>
2023-11-21 15:29:20 +0300

    Micro changes

--- src/main/java/ru/idr/arbitragestatistics/api/DocumentService.java
+++ src/main/java/ru/idr/arbitragestatistics/api/DocumentService.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import org.json.JSONArray;
